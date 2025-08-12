cbad3f7a6c37f240838f884926a8f68d38dd25d5
rosenclever <rosenclever@gmail.com>
2025-08-12 20:41:16 -0300

    descrição de exercício IRPF (sem solução fornecida)

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -3,3 +3,5 @@ string nome = Console.ReadLine();
 Console.WriteLine("Informe seu salário: ");
 decimal salario = decimal.Parse(Console.ReadLine());
 Console.WriteLine("Seja bem-vinda(o) {0}, o salário informado foi {1}", nome, salario);
+
+//informem o valor do IRPF a ser pago de acordo com o salário informado. Utilize a tabela real de desconto de IRPF
