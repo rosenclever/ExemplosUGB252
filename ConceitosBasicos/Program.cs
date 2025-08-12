59a03228a6fbe26a3e3c5d5a5e327a3d4aa6abc6
3174 <3174@LAB1-PROF>
2025-08-12 19:23:08 -0300

    Adicionar arquivos de projeto.

---
+++ ConceitosBasicos/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int idade = 24;
+Console.WriteLine("A idade é" + idade);
+if (idade >= 18)
+{
+    Console.WriteLine("Você pode tirar habilitação");
+}
